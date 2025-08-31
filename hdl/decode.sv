39b9987c58d5d47a45daeb3f571594a6518116af
Haani <haani@mit.edu>
2025-08-31 13:58:38 -0400

    decode now reads reg file and passes out values

--- hdl/decode.sv
+++ hdl/decode.sv
@@ -13,16 +13,65 @@ module decode (
   input wire execute_ready_in,
   output logic execute_valid_out,
   output DecodeResult execute_payload_out [SUPER_SCALAR_WIDTH-1:0]
+
+  output RegisterFileReadRequest register_file_reg0_read_request_out [SUPER_SCALAR_WIDTH-1:0],
+  output RegisterFileReadRequest register_file_reg1_read_request_out [SUPER_SCALAR_WIDTH-1:0],
+  input wire Word register_file_reg0_read_response_in [SUPER_SCALAR_WIDTH-1:0],
+  input wire Word register_file_reg1_read_response_in [SUPER_SCALAR_WIDTH-1:0]
 );
 
   logic rename_handshake [SUPER_SCALAR_WIDTH-1:0];
   logic fetch_handshake [SUPER_SCALAR_WIDTH-1:0];
 
   always_comb begin
-    fetch_ready_out = !execute_valid_out || execute_ready_in; // only signalled if not stalled by rename!!!!!!
+    fetch_ready_out = !execute_valid_out || execute_ready_in; // only signalled if not stalled by execute!!!!!!
 
     rename_handshake = execute_ready_in && execute_valid_out;
     fetch_handshake = fetch_ready_out && fetch_valid_in;
+  end
+
+  logic RegisterFileReadRequest source_register_1;
+  logic RegisterFileReadRequest source_register_2;
+  always_comb begin
+    for (int i = 0; i < SUPER_SCALAR_WIDTH; i=i+1) begin
+      case (fetch_data_in[i][3:0]) // opcode
+        JALR: begin
+          source_register_1[i] = fetch_data_in[i][15:10];
+        end
+        BRANCH: begin
+          source_register_1[i] = fetch_data_in[i][12:7];
+          source_register_2[i] = fetch_data_in[i][18:13];
+        end
+        LOAD: begin
+          source_register_1[i] = fetch_data_in[i][15:10];
+        end
+        STORE: begin
+          source_register_1[i] = fetch_data_in[i][9:4];
+          source_register_2[i] = fetch_data_in[i][15:10];
+        end
+        OP_IMM_NORMAL: begin
+          source_register_1[i] = fetch_data_in[i][18:13];
+        end
+        OP_IMM_SHIFT: begin
+          source_register_1[i] = fetch_data_in[i][18:13];
+        end
+        OP_NORMAL: begin
+          source_register_1[i] = fetch_data_in[i][18:13];
+          source_register_2[i] = fetch_data_in[i][24:19];
+        end
+        OP_SHIFT: begin
+          source_register_1[i] = fetch_data_in[i][18:13];
+          source_register_2[i] = fetch_data_in[i][24:19];
+        end
+        default: begin
+          source_register_1 <= 0;
+          source_register_2 <= 0;
+        end
+      endcase
+    end
+
+    register_file_reg0_read_request_out = source_register_1;
+    register_file_reg1_read_request_out = source_register_2;
   end
 
   Word program_counter;
@@ -57,7 +106,7 @@ module decode (
             JALR: begin
               execute_payload_out[i].instruction_type <= JALR;
               execute_payload_out[i].destination_register <= fetch_data_in[i][9:4];
-              execute_payload_out[i].source_register_1 <= fetch_data_in[i][15:10];
+              execute_payload_out[i].source_value_1 <= register_file_reg0_read_response_in[i];
               execute_payload_out[i].immediate <= {
                 16{fetch_data_in[i][24]},
                 fetch_data_in[i][24:16]
@@ -65,8 +114,8 @@ module decode (
             end
 
             BRANCH: begin
-              execute_payload_out[i].source_register_1 <= fetch_data_in[i][12:7];
-              execute_payload_out[i].source_register_2 <= fetch_data_in[i][18:13];
+              execute_payload_out[i].source_value_1 <= register_file_reg0_read_response_in[i];
+              execute_payload_out[i].source_value_2 <= register_file_reg1_read_response_in[i];
               execute_payload_out[i].immediate <= {
                 19{fetch_data_in[i][24]},
                 fetch_data_in[i][24:19]
@@ -106,7 +155,7 @@ module decode (
             LOAD: begin
               execute_payload_out[i].instruction_type <= LOAD;
               execute_payload_out[i].destination_register <= fetch_data_in[i][9:4];
-              execute_payload_out[i].source_register_1 <= fetch_data_in[i][15:10];
+              execute_payload_out[i].source_value_1 <= register_file_reg0_read_response_in[i];
               execute_payload_out[i].immediate <= {
                 16{fetch_data_in[i][24]},
                 fetch_data_in[i][24:16]
@@ -115,8 +164,8 @@ module decode (
 
             STORE: begin
               execute_payload_out[i].instruction_type <= STORE;
-              execute_payload_out[i].source_register_1 <= fetch_data_in[i][9:4];
-              execute_payload_out[i].source_register_2 <= fetch_data_in[i][15:10];
+              execute_payload_out[i].source_value_1 <= register_file_reg0_read_response_in[i];
+              execute_payload_out[i].source_value_2 <= register_file_reg1_read_response_in[i];
               execute_payload_out[i].immediate <= {
                 16{fetch_data_in[i][24]},
                 fetch_data_in[i][24:16]
@@ -125,7 +174,7 @@ module decode (
 
             OP_IMM_NORMAL: begin
               execute_payload_out[i].destination_register <= fetch_data_in[i][12:7];
-              execute_payload_out[i].source_register_1 <= fetch_data_in[i][18:13];
+              execute_payload_out[i].source_value_1 <= register_file_reg0_read_response_in[i];
               execute_payload_out[i].immediate <= {
                 19{fetch_data_in[i][24]},
                 fetch_data_in[i][24:19]
@@ -164,7 +213,7 @@ module decode (
 
             OP_IMM_SHIFT: begin
               execute_payload_out[i].destination_register <= fetch_data_in[i][12:7];
-              execute_payload_out[i].source_register_1 <= fetch_data_in[i][18:13];
+              execute_payload_out[i].source_value_1 <= register_file_reg0_read_response_in[i];
               execute_payload_out[i].immediate <= {
                 19{fetch_data_in[i][24]},
                 fetch_data_in[i][24:19]
@@ -191,8 +240,8 @@ module decode (
 
             OP_NORMAL: begin
               execute_payload_out[i].destination_register <= fetch_data_in[i][12:7];
-              execute_payload_out[i].source_register_1 <= fetch_data_in[i][18:13];
-              execute_payload_out[i].source_register_2 <= fetch_data_in[i][24:19];
+              execute_payload_out[i].source_value_1 <= register_file_reg0_read_response_in[i];
+              execute_payload_out[i].source_value_2 <= register_file_reg1_read_response_in[i];
 
               case (fetch_data_in[i][6:4])
                 3'b000: begin
@@ -227,8 +276,8 @@ module decode (
 
             OP_SHIFT: begin
               execute_payload_out[i].destination_register <= fetch_data_in[i][12:7];
-              execute_payload_out[i].source_register_1 <= fetch_data_in[i][18:13];
-              execute_payload_out[i].source_register_2 <= fetch_data_in[i][24:19];
+              execute_payload_out[i].source_value_1 <= register_file_reg0_read_response_in[i];
+              execute_payload_out[i].source_value_2 <= register_file_reg1_read_response_in[i];
 
               case (fetch_data_in[i][6:4])
                 3'b000: begin
